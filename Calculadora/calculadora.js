a9c561befadf7d7111922ed75ffd0e313fbe86d7
Patrícia Vasques <patriciahhhh@gmail.com>
2023-05-14 19:16:40 -0300

    função divisão da calculadora em  JavaScript

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -8,9 +8,10 @@ function subtrair (x, y){
 
 function multiplicar(x, y){
     return (x * y)
-}
+};
 
-
-
+function dividir(x, y){
+    return (x / y)
+};
 
 module.exports = {somar, subtrair, multiplicar}
--- Calculadora/index.js
+++ Calculadora/index.js
@@ -1,10 +1,13 @@
-const calculadora = require("/calculadora");
+const calculadora = require("./calculadora");
 
 const resultadoSoma = calculadora.somar(10, 5);
-console.log("resultado da soma é:", resultadoSoma);
+console.log("O resultado da soma é:", resultadoSoma);
 
 const resultadoSubtracao = calculadora.subtrair(125, 35);
-consolte.log("resultado da subtração é:", resultadoSubtracao);
+consolte.log("O resultado da subtração é:", resultadoSubtracao);
 
 const resultadoMultiplicacao = calculadora.multiplicar(8, 6);
-console.log("resultado da multiplicação é:", resultadoMultiplicacao);
+console.log("O resultado da multiplicação é:", resultadoMultiplicacao);
+
+const resultadoDivisao = calcular.dividir(28, 4);
+console.log("O resultado da divisão é:", resultadoDivisao);
